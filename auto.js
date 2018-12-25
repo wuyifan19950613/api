c4b6e85c2477cc8b7551200b5720aab1e5116e40
wuyifan1995 <502397665@qq.com>
2018-12-25 19:59:51 +0800

    优化

---
+++ auto.js
@@ -0,0 +1,13 @@
+let process = require('child_process');
+let fs = require('fs-extra');
+
+let ChildProcess  = process.fork('api.js');
+
+ChildProcess.on('exit',function (code) {
+    console.log('process exits + '+code);
+    fs.appendFileSync('./log.txt','线程退出');
+    if(code !== 0){
+        process.fork('./auto.js');
+    }
+
+});
